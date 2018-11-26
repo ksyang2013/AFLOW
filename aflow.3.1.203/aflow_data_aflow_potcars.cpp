a044457bad5f9074c34c38936700776f890c9300
Kesong YANG <kesong@ucsd.edu>
2018-11-25 16:00:42 -0800

    remove useless

--- aflow.3.1.203/aflow_data_aflow_potcars.cpp
+++
@@ -1,7 +0,0 @@
-// aflow_data_aflow_potcars.cpp automatic generated from AFLOW_PSEUDOPOTENTIALS.TXT
-#include <string>
-bool AFLOW_PSEUDOPOTENTIALS=0;
-std::string AFLOW_PSEUDOPOTENTIALS_TXT="\
-";
-std::string AFLOW_PSEUDOPOTENTIALS_LIST_TXT="\
-";
